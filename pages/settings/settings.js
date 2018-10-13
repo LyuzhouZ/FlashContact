af96cd1ec9d83d055d0e848c0fda19cc15b70c49
sotstas <sotstas@gmail.com>
2018-10-13 18:35:59 +0800

    Updated Settings

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,11 +1,40 @@
 // pages/settings/settings.js
 Page({
+    data: {
+      items: [
+        { name: 'USA', value: '美国'},
+        { name: 'CHN', value: '中国', checked: 'true'},
+        { name: 'BRA', value: '巴西'},
+        { name: 'JPN', value: '日本'},
+        { name: 'ENG', value: '英国'},
+        { name: 'TUR', value: '法国'},
+      ],
 
-  /**
-   * Page initial data
-   */
-  data: {
+      radioItems: [
+        { name: 'Share contact automatically', value: '0' },
+        { name: 'Ask for permission before sharing contact', value: '1', checked: true }
+      ],
 
+    },
+    checkboxChange: function(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+
+  submitFeedback: function(e) {
+    console.log('Feedback info：', e.detail.value)
   },
 
   /**
